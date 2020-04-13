cc9284e0126e430b0bd02d90f8c39d55dfe877d9
forrest23 <forrest23@126.com>
2020-04-13 13:40:05 +0800

    增加隐私政策

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,89 +10,89 @@
 </template>
 
 <script>
-import { Toast } from 'vant';
-import api from '../api';
+import { Toast } from 'vant'
+import api from '../api'
 export default {
   name: 'Login',
-  data() {
+  data () {
     return {
       msg: '锐龙4S店移动管理系统',
       companyname: '',
       username: '',
       password: '',
       show: false,
-      columns: [],
-    };
+      columns: []
+    }
   },
-  mounted: function() {
-    this.companyname = localStorage.getItem('companyname') || '';
-    this.username = localStorage.getItem('username') || '';
-    this.password = localStorage.getItem('password') || '';
+  mounted: function () {
+    this.companyname = localStorage.getItem('companyname') || ''
+    this.username = localStorage.getItem('username') || ''
+    this.password = localStorage.getItem('password') || ''
   },
   methods: {
-    getCompanyInfo() {
+    getCompanyInfo () {
       api.getCompanyInfo().then(
         response => {
           if (!response.ok) {
-            Toast('获取公司信息失败！');
+            Toast('获取公司信息失败！')
           }
-          var data = JSON.parse(response.bodyText);
+          var data = JSON.parse(response.bodyText)
           data.result.forEach(e => {
-            this.columns.push(e.fcmpname);
-          });
+            this.columns.push(e.fcmpname)
+          })
         },
         response => {
-          Toast('获取公司信息失败！请检查网络！');
+          Toast('获取公司信息失败！请检查网络！')
         }
-      );
+      )
     },
-    login() {
-      if (!this.username){
-          Toast('请输入用户名！');
-          return;
+    login () {
+      if (!this.username) {
+        Toast('请输入用户名！')
+        return
       }
-      if (!this.password){
-          Toast('请输入密码！');
-          return;
+      if (!this.password) {
+        Toast('请输入密码！')
+        return
       }
       Toast.loading({
         mask: true,
-        message: '登陆中...',
-      });
-      var userInfo = {};
-      userInfo.fcmpname = this.companyname;
-      userInfo.fname = this.username;
-      userInfo.fpwd = this.password;
+        message: '登陆中...'
+      })
+      var userInfo = {}
+      userInfo.fcmpname = this.companyname
+      userInfo.fname = this.username
+      userInfo.fpwd = this.password
       api.login(userInfo).then(
         response => {
           if (!response.ok) {
-            Toast('登录失败!请检查网络！');
+            Toast('登录失败!请检查网络！')
           }
           if (response.data.success) {
-            sessionStorage.setItem('companyno',response.data.companyNo);
-            sessionStorage.setItem('companyname', this.companyname);
-            sessionStorage.setItem('username', this.username);
-            localStorage.setItem('companyno', response.data.companyNo);
-            localStorage.setItem('companyname', this.companyname);
-            localStorage.setItem('username', this.username);
-            localStorage.setItem('password', this.password);
-            this.$router.push('/');
-            Toast.clear();
-          }else{
-            Toast('登录失败!用户名或密码错误！');
+            sessionStorage.setItem('companyno', response.data.companyNo)
+            sessionStorage.setItem('companyname', this.companyname)
+            sessionStorage.setItem('username', this.username)
+            localStorage.setItem('companyno', response.data.companyNo)
+            localStorage.setItem('companyname', this.companyname)
+            localStorage.setItem('username', this.username)
+            localStorage.setItem('password', this.password)
+            this.$router.push('/')
+            Toast.clear()
+          } else {
+            Toast('登录失败!用户名或密码错误！')
           }
         },
         response => {
-          Toast('登录失败!');
+          Toast('登录失败!')
         }
-      );
+      )
     },
-    onConfirm(value, index) {
-      this.companyname = value;
-      this.show = false;
-    },
-  },
-};
+    onConfirm (value, index) {
+      this.companyname = value
+      this.show = false
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -39,91 +39,102 @@
 
     <van-panel title="" desc="公众号提交绑定后，需要您同时店内提交申请。绑定后您会收到会员充值兑换变动微信提醒，并支持微信支付验证码功能。" >
    </van-panel>
+
+<van-row class="rowtop">
+  <van-col span="4"><van-switch v-model="checked" size="20px"/></van-col>
+  <van-col span="20" class="rowlabel">*本人同意将此表格的信息提供给丰田汽车（中国）投资有限公司，并同意此等信息可由丰田汽车（中国）投资有限公司向LEXUS雷克萨斯中国经销商、授权第三方（包括但不限于供应商、代理人等）共享。本人同意丰田汽车（中国）投资有限公司及其LEXUS雷克萨斯中国经销商向本人发送相关商业性信息。*丰田汽车（中国）投资有限公司保证按照<a href="https://cllexus.carcloud.xyz/lexus.pdf">《丰田汽车（中国）投资有限公司隐私政簧》</a>的规定及相关法律法规的规定，妥善使用消费者个人信息。</van-col>
+</van-row>
 </van-cell-group>
   </div>
 </template>
 
 <script>
-import { Toast, Dialog } from 'vant';
-import api from '../api';
+import { Toast, Dialog } from 'vant'
+import api from '../api'
 export default {
   name: 'Login',
-  data() {
+  data () {
     return {
       name: '',
       plate_num: '',
-      mobile:'',
+      mobile: '',
       openid: '',
-      cmpno:'',
-    };
+      cmpno: '',
+      checked: false
+    }
   },
-  mounted: function() {
-    this.openid = this.$cookies.get('openid');
-    this.cmpno = this.$route.query.cmpno;
+  mounted: function () {
+    this.openid = this.$cookies.get('openid')
+    this.cmpno = this.$route.query.cmpno
   },
   methods: {
-    blur(){
-      this.plate_num = this.plate_num.toUpperCase();
+    blur () {
+      this.plate_num = this.plate_num.toUpperCase()
     },
-    submit() {
+    submit () {
       if (!this.name) {
-        Toast('请先输入车主姓名！');
-        return;
+        Toast('请先输入车主姓名！')
+        return
       }
       if (!this.plate_num) {
-        Toast('请先输入车牌号码，无车牌号请输车架号后8位！');
-        return;
+        Toast('请先输入车牌号码，无车牌号请输车架号后8位！')
+        return
       }
-      if (this.plate_num.trim().length < 6 ) {
-        Toast('请先输入正确的车牌号码，无车牌号请输车架号后8位！');
-        return;
+      if (this.plate_num.trim().length < 6) {
+        Toast('请先输入正确的车牌号码，无车牌号请输车架号后8位！')
+        return
       }
       if (!this.mobile) {
-        Toast('请输入办理会员时登记的手机号码！');
-        return;
+        Toast('请输入办理会员时登记的手机号码！')
+        return
       }
-      if (this.mobile.trim().length !=11) {
-        Toast('请输入11位手机号码！');
-        return;
+      if (this.mobile.trim().length != 11) {
+        Toast('请输入11位手机号码！')
+        return
+      }
+      if (!this.checked) {
+        Toast('请先阅读并勾选隐私政策！')
+        return
       }
       if (!this.openid) {
-        Toast('获取微信用户信息失败，请关闭当前页面再试！');
-        return;
+        Toast('获取微信用户信息失败，请关闭当前页面再试！')
+        return
       }
+
       Toast.loading({
         mask: true,
-        message: '提交数据中...',
-      });
-      var member = {};
-      member.cmpno = this.cmpno;
-      member.openid = this.openid;
-      member.name = this.name;
-      member.licensenum = this.plate_num;
-      member.mobile = this.mobile;
+        message: '提交数据中...'
+      })
+      var member = {}
+      member.cmpno = this.cmpno
+      member.openid = this.openid
+      member.name = this.name
+      member.licensenum = this.plate_num
+      member.mobile = this.mobile
       api.updateOpenId(member).then(
         response => {
           if (!response.ok) {
-            Toast('车主信息绑定失败!请检查网络！');
+            Toast('车主信息绑定失败!请检查网络！')
           }
           if (response.data.success) {
-            Toast.clear();
+            Toast.clear()
             Dialog.alert({
-              message: '车主信息绑定成功！',
+              message: '车主信息绑定成功！'
             }).then(() => {
-              WeixinJSBridge.invoke('closeWindow',{},function(res){
-              });
-            });
+              WeixinJSBridge.invoke('closeWindow', {}, function (res) {
+              })
+            })
           } else {
-            Toast(response.data.message);
+            Toast(response.data.message)
           }
         },
         response => {
-          Toast('车主信息绑定失败!请检查网络');
+          Toast('车主信息绑定失败!请检查网络')
         }
-      );
-    },
-  },
-};
+      )
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -146,5 +157,15 @@ h2 {
 }
 .button {
   margin-top: 25px;
+}
+.rowtop {
+  margin-top: 10px;
+}
+.rowlabel{
+  color: #969799;
+  font-size: 12px;
+  margin-top: 3px;
+  line-height: 18px;
+  text-align: left;
 }
 </style>
